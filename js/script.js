c6a75f84bf32b84beab6c982b35bd4b40a59e48f
Andrea Pacifico <pacifico.mensa@gmail.com>
2019-12-18 15:17:59 +0100

    finito

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+
+var eta = parseInt(prompt('quanti anni hai?'));
+console.log(eta);
+var km = parseInt(prompt('quanti km devi percorrere?'));
+console.log(km);
+var prezzo = 0.21 * km;
+console.log(prezzo);
+if (eta < 18) {
+  prezzo = prezzo * 0.8;
+}
+else if (eta > 65) {
+  prezzo = prezzo * 0.6;
+}
+alert("il prezzo del biglietto è: " + prezzo + " €")
